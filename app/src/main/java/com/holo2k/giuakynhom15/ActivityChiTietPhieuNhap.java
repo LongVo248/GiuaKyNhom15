ca5419c90ac5dfd6f496b97014e29620f280767e
HQKhanhDaisy <huynhquockhanh0907@gmail.com>
2022-04-24 19:21:26 +0700

    Update calendar

--- app/src/main/java/com/holo2k/giuakynhom15/ActivityChiTietPhieuNhap.java
+++ app/src/main/java/com/holo2k/giuakynhom15/ActivityChiTietPhieuNhap.java
@@ -1,4 +1,52 @@
 package com.holo2k.giuakynhom15;
 
-public class ActivityChiTietPhieuNhap {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Calendar;
+
+public class ActivityChiTietPhieuNhap extends AppCompatActivity {
+    Button btnNgay;
+    TextView tvNgayNhapPhieu;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chi_tiet_phieu);
+        setControl();
+        setEvents();
+    }
+
+    private void setEvents() {
+        btnNgay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("\n\n\n\n helloooooooooooooooooooooooooo \n\n\n");
+                Toast.makeText(ActivityChiTietPhieuNhap.this, "Hello", Toast.LENGTH_LONG);
+                Calendar calendar = Calendar.getInstance();
+                int year = calendar.get(Calendar.YEAR);
+                int month = calendar.get(Calendar.MONTH);
+                int day = calendar.get(Calendar.DAY_OF_MONTH);
+                DatePickerDialog datePickerDialog = new DatePickerDialog(ActivityChiTietPhieuNhap.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+                        tvNgayNhapPhieu.setText(String.format("%d/%d/%d", i, i1, i2));
+                    }
+                }, year, month, day);
+                datePickerDialog.show();
+            }
+        });
+    }
+
+    private void setControl() {
+        tvNgayNhapPhieu = findViewById(R.id.tvNgayNhapPhieu);
+    }
 }
--- app/src/main/java/com/holo2k/giuakynhom15/ActivityThemPhieuNhap.java
+++ app/src/main/java/com/holo2k/giuakynhom15/ActivityThemPhieuNhap.java
@@ -1,10 +1,15 @@
 package com.holo2k.giuakynhom15;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,14 +18,16 @@ import com.holo2k.giuakynhom15.database.DBVatTu;
 import com.holo2k.giuakynhom15.model.ChiTietPhieuNhap;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 
 public class ActivityThemPhieuNhap extends AppCompatActivity {
     TextView tvMaPhieuThemPhieu;
     EditText editTenKhoChiTiet;
     TextView tvNgayNhapPhieu;
     ListView lvDSVatTuPhieu;
+    ImageView imgCalendar;
     Button btnLuuPhieu, btnThemVTThemPhieu, btnDatLai;
-    ArrayList<ChiTietPhieuNhap> chiTietPhieuNhapArrayList =  new ArrayList<>();
+    ArrayList<ChiTietPhieuNhap> chiTietPhieuNhapArrayList = new ArrayList<>();
     ThemPhieuAdapter themPhieuAdapter;
     DBVatTu dbChiTietPhieu;
 
@@ -31,7 +38,8 @@ public class ActivityThemPhieuNhap extends AppCompatActivity {
         setControls();
         setEvents();
     }
-    private void setControls(){
+
+    private void setControls() {
         tvMaPhieuThemPhieu = findViewById(R.id.tvMaPhieuThemPhieu);
         editTenKhoChiTiet = findViewById(R.id.editTenKhoChiTiet);
         tvNgayNhapPhieu = findViewById(R.id.tvNgayNhapPhieu);
@@ -39,9 +47,26 @@ public class ActivityThemPhieuNhap extends AppCompatActivity {
         btnDatLai = findViewById(R.id.btnDatLai);
         btnLuuPhieu = findViewById(R.id.btnLuuPhieu);
         btnThemVTThemPhieu = findViewById(R.id.btnThemVTthemPhieu);
+        imgCalendar = findViewById(R.id.imgCalendar);
     }
 
-    private void setEvents(){
+    private void setEvents() {
+        imgCalendar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar calendar = Calendar.getInstance();
+                int year = calendar.get(Calendar.YEAR);
+                int month = calendar.get(Calendar.MONTH);
+                int day = calendar.get(Calendar.DAY_OF_MONTH);
+                DatePickerDialog datePickerDialog = new DatePickerDialog(ActivityThemPhieuNhap.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+                        tvNgayNhapPhieu.setText(String.format("%d/%d/%d", i2, i1 + 1, i));
+                    }
+                }, year, month, day);
+                datePickerDialog.show();
+            }
+        });
 
     }
 }
